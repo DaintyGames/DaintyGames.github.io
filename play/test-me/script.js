3d1ab0082c1a0c021b0fd1434a9a426e248fcfc8
Dainty <games.dainty@gmail.com>
2022-06-01 21:12:43 +0300

    "play/test-me": scripts moved to file "script.js"

---
+++ play/test-me/script.js
@@ -0,0 +1,25 @@
+function show() {
+    var element = document.getElementById("description");
+    element.style.maxHeight  = "3000px";
+    var element = document.getElementById("read_text");
+    element.style.display = 'none';  
+    var element = document.getElementById("gradient");
+    element.style.display = 'none';  
+    var element = document.getElementById("сollapse");
+    element.style.display = 'block';  
+}
+
+function hide() {
+    var element = document.getElementById("description");
+    element.style.maxHeight  = "341px";
+    var element = document.getElementById("read_text");
+    element.style.display = 'block';  
+    var element = document.getElementById("gradient");
+    element.style.display = 'block';  
+    var element = document.getElementById("сollapse");
+    element.style.display = 'none';  
+}
+
+function installClick() {
+    window.alert("Oops! Something went wrong...");
+}
